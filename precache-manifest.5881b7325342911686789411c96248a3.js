bc922e82928895b7c9e42a8d80f3e0df8c87d6cc
Robin Zigmond <robinzig@hotmail.com>
2019-04-27 00:09:51 +0100

    Updates

---
+++ precache-manifest.5881b7325342911686789411c96248a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c046a39cfdbd7748a03",
+    "url": "/which-game-to-play/static/js/runtime~main.6c046a39.js"
+  },
+  {
+    "revision": "e68b6af8d706d0b0cf98",
+    "url": "/which-game-to-play/static/js/main.e68b6af8.chunk.js"
+  },
+  {
+    "revision": "14d7214d2e7be3047885",
+    "url": "/which-game-to-play/static/js/1.14d7214d.chunk.js"
+  },
+  {
+    "revision": "e68b6af8d706d0b0cf98",
+    "url": "/which-game-to-play/static/css/main.19346f79.chunk.css"
+  },
+  {
+    "revision": "6a4eb5336d480f74eda04b22680debee",
+    "url": "/which-game-to-play/index.html"
+  }
+];
